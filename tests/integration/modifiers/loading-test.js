41bb1f82e315a8c54edbbf1a92b2bea47a610037
Sinan Keskin <sinan@kesk.in>
2021-02-23 21:44:59 +0300

    Update some packages

--- tests/integration/modifiers/loading-test.js
+++ tests/integration/modifiers/loading-test.js
@@ -1,13 +1,14 @@
 import { module, test } from 'qunit';
-import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
-import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Modifier | loading', function(hooks) {
+import { hbs } from 'ember-cli-htmlbars';
+import { render } from '@ember/test-helpers';
+import { setupRenderingTest } from 'ember-qunit';
+
+module('Integration | Modifier | loading', function (hooks) {
   setupRenderingTest(hooks);
 
   // Replace this with your real tests.
-  test('it renders', async function(assert) {
+  test('it renders', async function (assert) {
     await render(hbs`<div {{loading}}></div>`);
 
     assert.ok(true);
