1777234981f7a39029d17c001fa3e3c9420cffb4
Sinan Keskin <sinan@kesk.in>
2021-02-23 21:42:21 +0300

    Update modifier

--- addon/modifiers/loading.js
+++ addon/modifiers/loading.js
@@ -6,7 +6,10 @@ export default class LoadingModifier extends Modifier {
   }
 
   didInstall() {
-    this._check();
+    if (!window.img_loading) {
+      this._check();
+    }
+
     this._setAttibutes();
   }
 
@@ -14,37 +17,41 @@ export default class LoadingModifier extends Modifier {
     if (window.img_loading === 'native') {
       if (this.args.named.src) {
         this.element.setAttribute('src', this.args.named.src);
-      } else if (this.args.named.srcset) {
+      }
+
+      if (this.args.named.srcset) {
         this.element.setAttribute('srcset', this.args.named.srcset);
       }
 
       this.element.setAttribute('loading', this.args.named.type);
     } else if (window.img_loading === 'lazysizes') {
+      if (this.args.named.sizes) {
+        this.element.dataset.sizes = this.args.named.sizes;
+      }
+
       if (this.args.named.src) {
         this.element.dataset.src = this.args.named.src;
-      } else if (this.args.named.srcset) {
+      }
+
+      if (this.args.named.srcset) {
         this.element.dataset.srcset = this.args.named.srcset;
       }
-    }
 
-    this.element.classList.add('lazyload');
+      this.element.classList.add('lazyload');
+    }
   }
 
   _check() {
-    if (window.img_loading) {
-      return;
+    if ('loading' in HTMLImageElement.prototype) {
+      window.img_loading = 'native';
     } else {
-      if ('loading' in HTMLImageElement.prototype) {
-        window.img_loading = 'native';
-      } else {
-        window.img_loading = 'lazysizes';
+      window.img_loading = 'lazysizes';
 
-        let script = document.createElement('script');
-        script.async = true;
-        script.src =
-          'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.0/lazysizes.min.js';
-        document.body.appendChild(script);
-      }
+      let script = document.createElement('script');
+      script.async = true;
+      script.src =
+        'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.0/lazysizes.min.js';
+      document.body.appendChild(script);
     }
   }
 }
